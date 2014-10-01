2b548158bd503afa2c5ce6f5575c0480e17fce9b
Lew Ayotte <lew@ithemes.com>
2014-10-01 10:56:41 -0500

    Taggign 1.0.9

--- exchange-addon-manual-purchases.php
+++ exchange-addon-manual-purchases.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Manual Purchases Add-on
- * Version: 1.0.8
+ * Version: 1.0.9
  * Description: Adds manual purchases functionality to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/manual-purchases/
  * Author: iThemes
